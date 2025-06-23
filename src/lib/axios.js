d14be3b06869c63f723e54b56e245222d58f5012
0ojireho0 <jeremiahquintano17@gmail.com>
2025-06-24 00:28:58 +0800

    done

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,11 +3,11 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const axios = Axios.create({
     baseURL: isProd ? process.env.NEXT_PUBLIC_PROD_API_KEY : process.env.NEXT_PUBLIC_API_KEY,
-    // headers: {
-    //     'X-Requested-With': 'XMLHttpRequest',
-    // },
-    // withCredentials: true,
-    // withXSRFToken: true
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+    },
+    withCredentials: true,
+    withXSRFToken: true
 })
 
 export default axios
