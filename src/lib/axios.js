282eaa579659df9eeda3e65aba3f88f8a0816560
0ojireho0 <jeremiahquintano17@gmail.com>
2025-06-24 00:27:22 +0800

    done

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,10 +3,10 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const axios = Axios.create({
     baseURL: isProd ? process.env.NEXT_PUBLIC_PROD_API_KEY : process.env.NEXT_PUBLIC_API_KEY,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-    withCredentials: true,
+    // headers: {
+    //     'X-Requested-With': 'XMLHttpRequest',
+    // },
+    // withCredentials: true,
     // withXSRFToken: true
 })
 
